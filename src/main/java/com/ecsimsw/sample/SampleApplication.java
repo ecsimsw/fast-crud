c45b5863d67acb2e088d990e4b67a1880d6968cc
ecsimsw <ecsimsw@gmail.com>
2024-03-23 13:11:56 +0900

    add maven compiler source

--- src/main/java/com/ecsimsw/sample/SampleApplication.java
+++ src/main/java/com/ecsimsw/sample/SampleApplication.java
@@ -3,6 +3,7 @@ package com.ecsimsw.sample;
 import com.ecsimsw.fastcrud.annotation.EnableCrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @EnableCrud
 @SpringBootApplication
@@ -10,6 +11,10 @@ public class SampleApplication {
 
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(SampleApplication.class);
-        app.run(args);
+        ConfigurableApplicationContext run = app.run(args);
+        String[] beanDefinitionNames = run.getBeanDefinitionNames();
+        for(var name : beanDefinitionNames) {
+            System.out.println(name);
+        }
     }
 }
